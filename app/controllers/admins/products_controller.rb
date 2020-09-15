dfdc200d395bc2f3aa375958bb7630daa7397f3e
=daihm-1041 <=hoang.minh.dai@sun-asterisk.com>
2020-09-15 17:02:05 +0700

    create product

---
+++ app/controllers/admins/products_controller.rb
@@ -0,0 +1,30 @@
+class Admins::ProductsController < Admins::BaseController
+  before_action :load_data, only: %i(new create)
+
+  def new
+    @product = Product.new
+    @product.images.build
+  end
+
+  def create
+    @product = Product.new product_params
+    if @product.save
+      flash[:success] = t ".product_created_success"
+      redirect_to admins_products_path
+    else
+      flash[:danger] = t ".product_create_failed"
+      render :new
+    end
+  end
+
+  private
+
+  def load_data
+    @categories = Category.all
+    @suppliers = Supplier.all
+  end
+
+  def product_params
+    params.require(:product).permit Product::PERMIT_ATTRIBUTES
+  end
+end
